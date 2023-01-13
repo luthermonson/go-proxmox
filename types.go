e6fb33eb0741430626823200df5d8fe2f83eff3d
Johann Queuniet <sub_code.git@queuniet.fr>
2023-01-13 21:39:26 +0100

    Fix typo

--- types.go
+++ types.go
@@ -465,7 +465,7 @@ type VirtualMachineConfig struct {
 	IPConfig9 string            `json:"ipconfig9,omitempty"`
 }
 
-func (vmc *VirtualMachineConfig) mergdeIndexedDevices(prefix string) map[string]string {
+func (vmc *VirtualMachineConfig) mergeIndexedDevices(prefix string) map[string]string {
 	deviceMap := make(map[string]string, 0)
 	t := reflect.TypeOf(*vmc)
 	v := reflect.ValueOf(*vmc)
@@ -492,84 +492,84 @@ func (vmc *VirtualMachineConfig) mergdeIndexedDevices(prefix string) map[string]
 
 func (vmc *VirtualMachineConfig) MergeIDEs() map[string]string {
 	if nil == vmc.IDEs {
-		vmc.IDEs = vmc.mergdeIndexedDevices("IDE")
+		vmc.IDEs = vmc.mergeIndexedDevices("IDE")
 	}
 	return vmc.IDEs
 }
 
 func (vmc *VirtualMachineConfig) MergeSCSIs() map[string]string {
 	if nil == vmc.SCSIs {
-		vmc.SCSIs = vmc.mergdeIndexedDevices("SCSI")
+		vmc.SCSIs = vmc.mergeIndexedDevices("SCSI")
 	}
 	return vmc.SCSIs
 }
 
 func (vmc *VirtualMachineConfig) MergeSATAs() map[string]string {
 	if nil == vmc.SATAs {
-		vmc.SATAs = vmc.mergdeIndexedDevices("SATA")
+		vmc.SATAs = vmc.mergeIndexedDevices("SATA")
 	}
 	return vmc.SATAs
 }
 
 func (vmc *VirtualMachineConfig) MergeNets() map[string]string {
 	if nil == vmc.Nets {
-		vmc.Nets = vmc.mergdeIndexedDevices("Net")
+		vmc.Nets = vmc.mergeIndexedDevices("Net")
 	}
 	return vmc.Nets
 }
 
 func (vmc *VirtualMachineConfig) MergeVirtIOs() map[string]string {
 	if nil == vmc.VirtIOs {
-		vmc.VirtIOs = vmc.mergdeIndexedDevices("VirtIO")
+		vmc.VirtIOs = vmc.mergeIndexedDevices("VirtIO")
 	}
 	return vmc.VirtIOs
 }
 
 func (vmc *VirtualMachineConfig) MergeUnuseds() map[string]string {
 	if nil == vmc.Unuseds {
-		vmc.Unuseds = vmc.mergdeIndexedDevices("Unused")
+		vmc.Unuseds = vmc.mergeIndexedDevices("Unused")
 	}
 	return vmc.Unuseds
 }
 
 func (vmc *VirtualMachineConfig) MergeSerials() map[string]string {
 	if nil == vmc.Serials {
-		vmc.Serials = vmc.mergdeIndexedDevices("Serial")
+		vmc.Serials = vmc.mergeIndexedDevices("Serial")
 	}
 	return vmc.Serials
 }
 
 func (vmc *VirtualMachineConfig) MergeUSBs() map[string]string {
 	if nil == vmc.USBs {
-		vmc.HostPCIs = vmc.mergdeIndexedDevices("USB")
+		vmc.HostPCIs = vmc.mergeIndexedDevices("USB")
 	}
 	return vmc.USBs
 }
 
 func (vmc *VirtualMachineConfig) MergeHostPCIs() map[string]string {
 	if nil == vmc.HostPCIs {
-		vmc.HostPCIs = vmc.mergdeIndexedDevices("HostPCI")
+		vmc.HostPCIs = vmc.mergeIndexedDevices("HostPCI")
 	}
 	return vmc.HostPCIs
 }
 
 func (vmc *VirtualMachineConfig) MergeNumas() map[string]string {
 	if nil == vmc.Numas {
-		vmc.HostPCIs = vmc.mergdeIndexedDevices("Numa")
+		vmc.HostPCIs = vmc.mergeIndexedDevices("Numa")
 	}
 	return vmc.Numas
 }
 
 func (vmc *VirtualMachineConfig) MergeParallels() map[string]string {
 	if nil == vmc.Parallels {
-		vmc.Parallels = vmc.mergdeIndexedDevices("Parallel")
+		vmc.Parallels = vmc.mergeIndexedDevices("Parallel")
 	}
 	return vmc.Parallels
 }
 
 func (vmc *VirtualMachineConfig) MergeIPConfigs() map[string]string {
 	if nil == vmc.IPConfigs {
-		vmc.IPConfigs = vmc.mergdeIndexedDevices("IPConfig")
+		vmc.IPConfigs = vmc.mergeIndexedDevices("IPConfig")
 	}
 	return vmc.IPConfigs
 }
