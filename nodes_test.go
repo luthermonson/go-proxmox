37d0caadb112464943857328b2971abc38014f8f
Luther Monson <luther.monson@gmail.com>
2023-09-08 21:18:16 -0700

    add node and node version test (#94)

--- nodes_test.go
+++ nodes_test.go
@@ -20,3 +20,22 @@ func TestClient_Nodes(t *testing.T) {
 	}
 	//assert.Equal(t, 6, len(testData))
 }
+
+func TestClient_Node(t *testing.T) {
+	mocks.On(mockConfig)
+	defer mocks.Off()
+	client := mockClient()
+
+	node, err := client.Node("node1")
+	assert.Nil(t, err)
+	assert.Equal(t, "node1", node.Name)
+	assert.NotNil(t, node.client)
+
+	v, err := node.Version()
+	assert.Nil(t, err)
+	assert.Equal(t, "7.4", v.Release)
+
+	node, err = client.Node("doesntexist")
+	assert.NotNil(t, err)
+	assert.Nil(t, node)
+}
--- tests/mocks/pve7x/nodes.go
+++ tests/mocks/pve7x/nodes.go
@@ -761,4 +761,73 @@ func nodes() {
     }
 }`)
 
+	gock.New(config.C.URI).
+		Persist().
+		Get("^/nodes/node1/status$").
+		Reply(200).
+		JSON(`{
+    "data": {
+        "idle": 0,
+        "cpu": 0.00260552371026576,
+        "ksm": {
+            "shared": 0
+        },
+        "swap": {
+            "total": 0,
+            "free": 0,
+            "used": 0
+        },
+        "pveversion": "pve-manager/7.4-16/0f39f621",
+        "wait": 0,
+        "uptime": 2501631,
+        "kversion": "Linux 5.15.108-1-pve #1 SMP PVE 5.15.108-2 (2023-07-20T10:06Z)",
+        "cpuinfo": {
+            "mhz": "3400.000",
+            "model": "Intel(R) Core(TM) i7-6700 CPU @ 3.40GHz",
+            "sockets": 1,
+            "flags": "fpu vme de pse tsc msr pae mce cx8 apic sep mtrr pge mca cmov pat pse36 clflush dts acpi mmx fxsr sse sse2 ss ht tm pbe syscall nx pdpe1gb rdtscp lm constant_tsc art arch_perfmon pebs bts rep_good nopl xtopology nonstop_tsc cpuid aperfmperf pni pclmulqdq dtes64 monitor ds_cpl vmx smx est tm2 ssse3 sdbg fma cx16 xtpr pdcm pcid sse4_1 sse4_2 x2apic movbe popcnt aes xsave avx f16c rdrand lahf_lm abm 3dnowprefetch cpuid_fault invpcid_single pti tpr_shadow vnmi flexpriority ept vpid ept_ad fsgsbase tsc_adjust bmi1 hle avx2 smep bmi2 erms invpcid rtm mpx rdseed adx smap clflushopt intel_pt xsaveopt xsavec xgetbv1 xsaves dtherm ida arat pln pts hwp hwp_notify hwp_act_window hwp_epp",
+            "hvm": "1",
+            "cores": 4,
+            "user_hz": 100,
+            "cpus": 8
+        },
+        "loadavg": [
+            "0.00",
+            "0.00",
+            "0.00"
+        ],
+        "memory": {
+            "total": 65919459328,
+            "free": 57824059392,
+            "used": 8095399936
+        },
+        "rootfs": {
+            "total": 948338819072,
+            "free": 937851224064,
+            "used": 10487595008,
+            "avail": 937851224064
+        }
+    }
+}`)
+
+	gock.New(config.C.URI).
+		Persist().
+		Get("^/nodes/doesntexist/status$").
+		Reply(500).
+		JSON(`{
+    "data": null
+}`)
+
+	gock.New(config.C.URI).
+		Persist().
+		Get("^/nodes/node1/version$").
+		Reply(200).
+		JSON(`{
+    "data": {
+        "release": "7.4",
+        "version": "7.4-16",
+        "repoid": "0f39f621"
+    }
+}`)
+
 }
