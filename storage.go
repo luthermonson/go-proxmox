f910ba6fa1215fbce631eda45ffab56f2bce4171
alperencelik <celikal18@itu.edu.tr>
2024-02-22 20:49:20 +0300

    ✨feat: Add DeleteContent method for storage

--- storage.go
+++ storage.go
@@ -112,6 +112,15 @@ func (s *Storage) GetContent(ctx context.Context) (content []*StorageContent, er
 	return content, err
 }
 
+func (s *Storage) DeleteContent(ctx context.Context, content string) (*Task, error) {
+	var upid UPID
+	err := s.client.Delete(ctx, fmt.Sprintf("/nodes/%s/storage/%s/content/%s", s.Node, s.Name, content), &upid)
+	if err != nil {
+		return nil, err
+	}
+	return NewTask(upid, s.client), nil
+}
+
 func (s *Storage) ISO(ctx context.Context, name string) (iso *ISO, err error) {
 	err = s.client.Get(ctx, fmt.Sprintf("/nodes/%s/storage/%s/content/%s:%s/%s", s.Node, s.Name, s.Name, "iso", name), &iso)
 	if err != nil {
