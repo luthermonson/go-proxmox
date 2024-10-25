0d48950b18b1e33c51530e3b33dff93c7998c83d
Pivnoy <evgenij_makarev20021@mail.ru>
2024-10-25 11:57:56 +0300

    ha resource get & list

--- cluster_ha_resources.go
+++ cluster_ha_resources.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-func (cl *Cluster) HAResourceCreate(ctx context.Context, haResource HAResource) error {
+func (cl *Cluster) CreateHAResource(ctx context.Context, haResource HAResource) error {
 	if err := cl.client.Post(ctx, "/cluster/ha/resources", &haResource, nil); err != nil {
 		return err
 	}
@@ -13,10 +13,28 @@ func (cl *Cluster) HAResourceCreate(ctx context.Context, haResource HAResource) 
 	return nil
 }
 
-func (cl *Cluster) HAResourceDelete(ctx context.Context, sid SID) error {
+func (cl *Cluster) DeleteHAResource(ctx context.Context, sid SID) error {
 	if err := cl.client.Delete(ctx, fmt.Sprintf("/cluster/ha/resources/%s:%d", sid.Type, sid.ID), nil); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (cl *Cluster) GetHAResource(ctx context.Context, sid SID) (HAResource, error) {
+	var haResource HAResource
+	if err := cl.client.Get(ctx, fmt.Sprintf("/cluster/ha/resources/%s:%d", sid.Type, sid.ID), &haResource); err != nil {
+		return haResource, err
+	}
+
+	return haResource, nil
+}
+
+func (cl *Cluster) ListHAResources(ctx context.Context, resourceType HAResourceType) ([]SID, error) {
+	var haResources []SID
+	if err := cl.client.Get(ctx, fmt.Sprintf("/cluster/ha/resources?type=%s", resourceType), &haResources); err != nil {
+		return nil, err
+	}
+
+	return haResources, nil
 }
