ae576a85ccea85b3353f5d808cc64d1244d34bed
Erik [Bad Sector Labs] <555113-badsectorlabs-erik@users.noreply.gitlab.com>
2025-12-05 13:24:43 -0500

    refactor(pools): 🔨 update the Pool function to use the modern endpoint
    Querying pools by path value (/pools/{poolid}) is deprecated and the new way is by query parameter (/pools/?poolid={poolid}). This supports nested pools. As the return value of /pools/ is always an array, the Pool function was modified to extract the single pool result and return it.
    
    Signed-off-by: Erik [Bad Sector Labs] <555113-badsectorlabs-erik@users.noreply.gitlab.com>

--- pools.go
+++ pools.go
@@ -24,20 +24,36 @@ func (c *Client) Pools(ctx context.Context) (pools Pools, err error) {
 
 // Pool optional filter of cluster resources by type, enum can be "qemu", "lxc", "storage".
 func (c *Client) Pool(ctx context.Context, poolid string, filters ...string) (pool *Pool, err error) {
-	u := url.URL{Path: fmt.Sprintf("/pools/%s", poolid)}
+	u := url.URL{Path: "/pools/"}
+
+	// /pools/{poolid} is deprecated as it does not support nested pools
+	// so we're using the /pools/ endpoint with a query parameter as recommended by the API documentation
+	params := url.Values{}
+	params.Add("poolid", poolid)
 
 	// filters are variadic because they're optional, munging everything passed into one big string to make
 	// a good request and the api will error out if there's an issue
 	if f := strings.Replace(strings.Join(filters, ""), " ", "", -1); f != "" {
-		params := url.Values{}
 		params.Add("type", f)
-		u.RawQuery = params.Encode()
 	}
 
-	if err = c.Get(ctx, u.String(), &pool); err != nil {
+	u.RawQuery = params.Encode()
+
+	var pools Pools
+	if err = c.Get(ctx, u.String(), &pools); err != nil {
 		return nil, err
 	}
-	pool.PoolID = poolid
+
+	if len(pools) == 0 {
+		// This will not hit as the API will return a 500 error if the pool does not exist
+		return nil, fmt.Errorf("pool not found")
+	} else if len(pools) == 1 {
+		pool = pools[0]
+	} else {
+		// Should be impossible to have multiple pools with the same poolid
+		return nil, fmt.Errorf("multiple pools found for poolid: %s", poolid)
+	}
+
 	pool.client = c
 
 	return
